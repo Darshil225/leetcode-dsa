9183cb5ace95405228db4be41f7ecc2eabcc1ff5
Darshil225 <amindarshil848@gmail.com>
2026-01-22 14:30:29 +0530

    Time: 1601 ms (5.35%) | Memory: 28.1 MB (86.40%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -2,32 +2,23 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
-        int el = 0, cnt = 0;
+       
+       for(int i=0;i<n;i++){
+       int cnt=0;
+       
 
-        // Phase 1: Find a candidate using Boyer-Moore Voting Algorithm
-        for (int i = 0; i < n; i++) {
-            if (cnt == 0) {
-                el = nums[i];
-                cnt = 1;
-            } else if (nums[i] == el) {
-                cnt++;
-            } else {
-                cnt--;
-            }
+       for(int j=0;j<n;j++){
+        if(nums[i]==nums[j]){
+            cnt++;
         }
+       }
 
-        // Phase 2: Verify if the candidate is actually the majority
-        int cnt1 = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == el) {
-                cnt1++;
-            }
-        }
+       if(cnt>n/2){
+        return nums[i];
+       }
 
-        if (cnt1 > n / 2) {
-            return el;
-        }
 
-        return -1;  // No majority element found
     }
+    return -1;
+}
 };
